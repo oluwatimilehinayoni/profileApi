32f2f313cc7d2f4de3b9e60d8cafebefc2329ea5
oluwatimilehinayoni <ayonitimilehin58@gmail.com>
2022-11-05 22:54:06 +0100

    first commit

---
+++ app/Http/Controllers/Api/ProfileController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $profile = Profile::all();
+
+        return response()->json([
+            'status' => true,
+            'profile' => $profile
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $profile = Profile::create($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Profile created successfully',
+            'profile' => $profile
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profile $profile)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profile $profile)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profile  $profile
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profile $profile)
+    {
+        if(!$profile->delete()){
+            return  response()->json([
+                'status' => false,
+                'messaage' => 'Profile could not be deleted!'
+            ], 504);
+        }
+
+        return  response()->json([
+            'status' => true,
+            'messaage' => 'Profile deleted successfully!'
+        ], 200);
+    }
+}
---
+++ database/migrations/2022_11_05_182212_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('slackUsername');
+            $table->string('bio');
+            $table->boolean('backend')->nullable()->default(true);
+            $table->integer('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
